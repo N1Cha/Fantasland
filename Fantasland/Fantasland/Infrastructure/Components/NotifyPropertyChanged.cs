dc7a72b15745d9087795f348258f69ff964bcfbe
n.chalakov <nsch_19@abv.bg>
2019-09-02 19:25:11 +0300

    Add NotifyPropertyChanged and new Data project

---
+++ Fantasland/Fantasland/Infrastructure/Components/NotifyPropertyChanged.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasland.Infrastructure
+{
+    public class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyChanged(string prop)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
