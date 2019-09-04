1ea8c631fe7e4a84df8d87edf22c3ea9ef8cf903
n.chalakov <nsch_19@abv.bg>
2019-09-04 22:42:10 +0300

    Add properties in Products model and fix the border of the FullStock DataGrid

--- Fantasland/Data/Models/Product.cs
+++ Fantasland/Data/Models/Product.cs
@@ -3,14 +3,17 @@
     public class Product
     {
         public int Id { get; set; }
-         
+
         public string Name { get; set; }
-         
+
         public string Code { get; set; }
-        
+
         public double Price { get; set; }
 
         public int Quantity { get; set; }
 
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set;}
     }
 }
